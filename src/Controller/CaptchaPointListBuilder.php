6bab90c4b97f87caddcbd3d0e3f6c5c90ae98107
Fabiano Sant'Ana <fabiano@chuva-inc.com>
2019-10-04 18:55:59 -0300

    Issue #3085969 by wundo: Remove deprecated code

--- src/Controller/CaptchaPointListBuilder.php
+++ src/Controller/CaptchaPointListBuilder.php
@@ -14,7 +14,6 @@ class CaptchaPointListBuilder extends ConfigEntityListBuilder {
    * {@inheritdoc}
    */
   public function buildHeader() {
-    module_load_include('inc', 'captcha');
     $header['form_id'] = $this->t('Captcha Point form ID');
     $header['captcha_type'] = $this->t('Captcha Point challenge type');
 
